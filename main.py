f31dda59f3e7d7d1965a31a14c90a58dea78d254
Korzeremi02 <r.korzeniowski@icloud.com>
2024-04-29 14:22:45 +0200

    ci: adding interaction system

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.members =True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 riot_key = os.getenv("RIOT_KEY")
 discord_key = os.getenv("DISCORD_KEY")
@@ -17,41 +17,41 @@ user_data = {}
 
 # Faire help et init
 
-@bot.command()
-async def helpme(ctx,):
-    await ctx.send(f"Aide TeamTracker \n\nCommandes : \n**!helpme @TeamTracker** : Afficher aide bot TT \n**!ping @TeamTracker** : Ping TT \n**!define @MentionDiscord NomUtilisateurRiot Region @TeamTracker** : Ajouter utilisateur discord dans TT \n**!display @TeamTracker** : Afficher utilisateur TT \n**!erase @TeamTracker** : Effacer mémoire TT \n**!stats @MentionDiscord @TeamTracker** : Afficher stats actuelles mention Discord ")
-
-@bot.command()
-async def ping(ctx):
-  await ctx.send(f"TftTracker est bien opérationnel !")
-
-@bot.command()
-async def define(ctx, member: discord.Member, riot_name: str, region: str):
-    user_data[member.id] = {"riot_name": riot_name, "region": region}
-    await ctx.send(f"{member.display_name} : Riot username = {riot_name}; Region = {region}")
-
-@bot.command()
-async def display(ctx):
-    await ctx.send(user_data)
-
-@bot.command()
-async def erase(ctx):
-    global user_data
-    await ctx.send(f"Mémoire effacée !")
-    user_data = {}
-
-@bot.command()
-async def stats(ctx, member: discord.Member):
-    if member.id in user_data:
-        data = user_data[member.id]
-        await ctx.send(f"Informations pour {member.display_name} : Nom d'utilisateur Riot = {data['riot_name']}, Région = {data['region']}")
-    else:
-        await ctx.send(f"Aucune information trouvée pour {member.display_name}. \nMerci de l'ajouter via la commande suivante : \n**!define @Mention NomUtilisateurRiot Region @TeamTracker**")
-
-
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(name="!helpme @TeamTracker"))
-    print(f"Bot connecté en tant que {bot.user.name} ({bot.user.id})")
+    await bot.change_presence(activity=discord.Game(name="/help"))
+    synced = await bot.tree.sync()
+    print(f"Synced {len(synced)} command(s)")
+
+@bot.tree.command(name="help", description="help")
+async def helpme(interaction):
+    await interaction.response.send_message(f"Aide TeamTracker \n\nCommandes : \n**/helpme ** : Afficher aide bot TT \n**/ping ** : Ping TT \n**/define @MentionDiscord NomUtilisateurRiot Region ** : Ajouter utilisateur discord dans TT \n**/display ** : Afficher utilisateur TT \n**/erase ** : Effacer mémoire TT \n**/stats @MentionDiscord ** : Afficher stats actuelles mention Discord ")
+
+@bot.tree.command(name="ping", description="ping")
+async def ping(interaction):
+  await interaction.response.send_message(f"TftTracker est bien opérationnel !")
+
+@bot.tree.command(name="define", description="description")
+async def define(interaction, member: discord.Member, riot_name: str, region: str):
+    user_data[member.id] = {"riot_name": riot_name, "region": region}
+    await interaction.response.send_message(f"{member.display_name} : Riot username = {riot_name}; Region = {region}")
+
+@bot.tree.command(name="display", description="display")
+async def display(interaction):
+    await interaction.response.send_message(user_data)
+
+@bot.tree.command(name="erase", description="erase")
+async def erase(interaction):
+    global user_data
+    await interaction.response.send_message(f"Mémoire effacée !")
+    user_data = {}
+
+@bot.tree.command(name="stats", description="stats")
+async def stats(interaction, member: discord.Member):
+    if member.id in user_data:
+        data = user_data[member.id]
+        await interaction.response.send_message(f"Informations pour {member.display_name} : Nom d'utilisateur Riot = {data['riot_name']}, Région = {data['region']}")
+    else:
+        await interaction.response.send_message(f"Aucune information trouvée pour {member.display_name}. \nMerci de l'ajouter via la commande suivante : \n**!define @Mention NomUtilisateurRiot Region **")
 
 bot.run(discord_key)
