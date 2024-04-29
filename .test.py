db9f576ad5fa13006fc96751fcfb667abebb079b
Korzeremi02 <r.korzeniowski@icloud.com>
2024-04-29 16:59:03 +0200

    ci: modifying logic

--- .test.py
+++
@@ -1,8 +0,0 @@
-# ----------------------------- TEST
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello')
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import requests
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
@@ -13,27 +14,43 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 riot_key = os.getenv("RIOT_KEY")
 discord_key = os.getenv("DISCORD_KEY")
 
+user_id = {}
+user_secret = {}
 user_data = {}
+user_profile = {}
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name="/help"))
-    autoTracker.start()
+    # autoTracker.start()
     synced = await bot.tree.sync()
     print(f"Synchronisation de {len(synced)} commandes")
 
 @bot.tree.command(name="help", description="Afficher l'aide du bot TeamTracker")
 async def helpme(interaction):
-    await interaction.response.send_message(f"Aide TeamTracker \n\nCommandes : \n**/helpme ** : Afficher aide bot TT \n**/ping ** : Ping TT \n**/define @MentionDiscord NomUtilisateurRiot Region ** : Ajouter utilisateur discord dans TT \n**/display ** : Afficher utilisateur TT \n**/erase ** : Effacer mémoire TT \n**/stats @MentionDiscord ** : Afficher stats actuelles mention Discord ")
+    await interaction.response.send_message(f"Aide TeamTracker \n\nCommandes : \n**/helpme ** : Afficher aide bot TT \n**/ping ** : Ping TT \n**/define ** : Ajouter utilisateur discord dans TT \n**/display ** : Afficher utilisateur TT \n**/erase ** : Effacer mémoire TT \n**/infos** : Afficher stats actuelles mention Discord ")
 
 @bot.tree.command(name="ping", description="Effectuer un ping vers le bot TeamTracker")
 async def ping(interaction):
   await interaction.response.send_message(f"TftTracker est bien opérationnel !")
 
 @bot.tree.command(name="define", description="Ajouter un utilisateur Discord à TeamTracker")
-async def define(interaction, member: discord.Member, riot_name: str, region: str, status: bool):
-    user_data[member.id] = {"riot_name": riot_name, "region": region, "status": status}
-    await interaction.response.send_message(f"{member.display_name} : Riot username = {riot_name}; Region = {region}; Status = {status}")
+async def define(interaction, member: discord.Member, riot_name: str, tag: str, region: str, status: bool):
+    global user_secret
+    user_data[member.id] = {"riot_name": riot_name, "tag": tag, "region": region, "status": status}
+    puid = 'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + riot_name + "/" + tag + '?api_key=' + riot_key
+    res = requests.get(puid, timeout = 127)
+    user_secret[member.id] = [dict(res.json())]
+    puid = user_secret[member.id][0]['puuid']
+    id = 'https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/' + puid + '?api_key=' + riot_key
+    res2 = requests.get(id, timeout=127)
+    user_id[member.id] = [dict(res2.json())]
+    user_secret = {}
+    await interaction.response.send_message(f"Id du membre Discord : {member.display_name} \nRiot username = {riot_name}\nTag = {tag}\nRegion = {region}\nStatus = {status}")
+
+@bot.tree.command(name="showsecret", description="show")
+async def showsecret(interaction):
+    await interaction.response.send_message(user_secret)
 
 @bot.tree.command(name="display", description="Afficher les données de TeamTracker")
 async def display(interaction):
@@ -45,17 +62,44 @@ async def erase(interaction):
     await interaction.response.send_message(f"Mémoire effacée !")
     user_data = {}
 
-@tasks.loop(seconds = 60)
-async def autoTracker():
-    print("autoTracker")
+@bot.tree.command(name="infos", description="Afficher les statistiques générales de la mention")
+async def infos(interaction, member: discord.Member):
+    id = user_id[member.id][0]['id']
+    profile_data = 'https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/' + id + '?api_key=' + riot_key
+    res = requests.get(profile_data, timeout=127)
+    user_profile = res.json()
+    for item in user_profile:
+        temp_dict = {
+            "queueType": item["queueType"],
+            "tier": item["tier"],
+            "rank": item["rank"],
+            "leaguePoints": item["leaguePoints"],
+            "wins": item["wins"],
+            "losses": item["losses"]
+        }
+    await interaction.response.send_message(temp_dict)
 
-# @bot.tree.command(name="stats", description="Afficher les statistiques générales de la mention")
-# async def stats(interaction, member: discord.Member):
-#     if member.id in user_data:
-#         data = user_data[member.id]
-#         await interaction.response.send_message(f"Informations pour {member.display_name} : Nom d'utilisateur Riot = {data['riot_name']}, Région = {data['region']}")
-#     else:
-#         await interaction.response.send_message(f"Aucune information trouvée pour {member.display_name}. \nMerci de l'ajouter via la commande suivante : \n**!define @Mention NomUtilisateurRiot Region **")
+@bot.tree.command(name="infosdoubleup", description="Afficher les statistiques générales de la mention")
+async def infosdoubleup(interaction, member: discord.Member):
+    id = user_id[member.id][0]['id']
+    profile_data = 'https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/' + id + '?api_key=' + riot_key
+    res = requests.get(profile_data, timeout=127)
+    user_profile = res.json()
+    for item in user_profile:
+        temp_dict = {
+            "queueType": item["queueType"],
+            "tier": item["tier"],
+            "rank": item["rank"],
+            "leaguePoints": item["leaguePoints"],
+            "wins": item["wins"],
+            "losses": item["losses"]
+        }
+        await interaction.response.send_message(temp_dict)
+
+# @tasks.loop(seconds = 60)
+# async def autoTracker():
+#     print("autoTracker")
+
 
 bot.run(discord_key)
 
